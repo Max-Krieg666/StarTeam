f337ebb3e677d8dddf02067f45e3b0f42123f2f4
Max-Krieg666 <maxon_B@mail.ru>
2015-06-25 00:47:06 +0400

    seeds and tables view upd.

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,6 +3,7 @@ class Player < ActiveRecord::Base
   belongs_to :country
 
   validates :name, presence: true, uniqueness: true, length: {maximum: 50}
+  validates :country_id, presence: true
   validates :position1, presence: true, inclusion: { in: @@posit }
   validates :position2, inclusion: { in: [""]+@@posit }
   validates :talent, presence: true, inclusion: { in: 1..9 }
--- app/models/player_in_team.rb
+++ app/models/player_in_team.rb
@@ -4,6 +4,7 @@ class PlayerInTeam < ActiveRecord::Base
   belongs_to :country
 
   validates :name, presence: true, uniqueness: true, length: {maximum: 50}
+  validates :country_id, presence: true
   validates :position1, presence: true, inclusion: { in: @@posit }
   validates :position2, inclusion: { in: [""]+@@posit }
   validates :talent, presence: true, inclusion: { in: 1..9 }
