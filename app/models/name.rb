e267083a1fc5b93cdac281f2dae33612b775dde6
Богоявленский Максим Владимирович <bogoyavlenskiy@a-soft.org>
2016-08-30 15:49:45 +0300

    remove 2 names &lastnames todo

--- app/models/name.rb
+++ app/models/name.rb
@@ -1,22 +1,22 @@
 class Name
-	# TODO доделать эту заготовку генерации!!!
+  # TODO доделать эту заготовку генерации!!!
   attr_accessor :fullname, :firstname, :prefix, :lastname, :country_id
 
   def initialize(country_id)
-  	@country_id = country_id
+    @country_id = country_id
   end
 
   def rand_name
-    
-  end
-
-  def parse_file
-
+    files = define_file
+    file1 = YAML.load_file(Rails.root.join('lib', 'names', files[0]))
+    file2 = YAML.load_file(Rails.root.join('lib', 'lastnames', files[1]))
+    name = file1[rand(file1.size)] + ' ' + file2[rand(file2.size)]
+    name
   end
 
   def define_file
-  	file_names = nil
-  	file_lastnames = nil
+    file_names = nil
+    file_lastnames = nil
     case @country_id
     when 239, 1, 7, 10, 15, 17, 20, 23
       # Англия, Австралия, Ангилья, Антигуа и Барбуда, Багамы, Барбадос, Белиз, Бермуды
@@ -63,9 +63,9 @@ class Name
       file_names = 'afghan'
       file_lastnames = 'afghan'
     when 16
-      # Бангладеш TODO!!!
-      file_names = '???'
-      file_lastnames = '???'
+      # Бангладеш
+      file_names = 'bangladesh'
+      file_lastnames = 'bangladesh'
     when 18, 99, 143, 144
       # Бахрейн, Кувейт, Объединенные Арабские Эмираты, Оман
       file_names = 'arabic'
@@ -84,8 +84,8 @@ class Name
       file_lastnames = 'belgian'
     when 22
       # Бенин TODO!!!
-      file_names = '???'
-      file_lastnames = '???'
+      file_names = 'beninese'
+      file_lastnames = 'beninese'
     when 24
       # Болгария
       file_names = 'bulgarian'
@@ -104,5 +104,6 @@ class Name
 
     file_names += '_names.yml'
     file_lastnames += '_lastnames.yml'
+    [file_names, file_lastnames]
   end
 end
