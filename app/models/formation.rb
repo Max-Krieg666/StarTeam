785e6bf604d85f51fcde03b1e8985a3af37e501e
Maxim Bogoyavlenskiy <m.bogoyavlenskiy@balance-pl.ru>
2019-04-11 13:36:35 +0300

    refactor team generation with formation

--- app/models/formation.rb
+++ app/models/formation.rb
@@ -9,4 +9,65 @@
 
 class Formation < ApplicationRecord
   has_many :teams, inverse_of: :formation
+
+  def with_reserve_players_before_generation
+    positions_count = {
+      'gk': 0,
+      'ld': 0,
+      'cd': 0,
+      'rd': 0,
+      'lm': 0,
+      'cm': 0,
+      'rm': 0,
+      'lf': 0,
+      'cf': 0,
+      'rf': 0
+    }
+    positions_count.each do |pos, _|
+      positions_count[pos] = number_of_players_for_position(pos)
+    end
+    boost_players_per_position(positions_count)
+  end
+
+  def number_of_players_for_position(position)
+    schema.scan(position).size
+  end
+
+  def boost_players_per_position(positions_count)
+    zero_players_per_pos = []
+    one_player_per_pos = []
+    two_players_per_pos = []
+    three_players_per_pos = []
+    positions_count.each do |pos, count|
+      case count
+      when 1
+        one_player_per_pos << { pos: count }
+      when 2
+        two_players_per_pos << { pos: count }
+      when 3
+        three_players_per_pos << { pos: count }
+      else # 0
+        zero_players_per_pos << { pos: count }
+      end
+    end
+    unallocated_players = 7
+    three_players_per_pos.each do |p|
+      value_boost = rand(1...2)
+      positions_count[p.key] = p.value + value_boost
+      unallocated_players -= value_boost
+    end
+    two_players_per_pos.each do |p|
+      break if unallocated_players.zero?
+      value_boost = 1
+      positions_count[p.key] = p.value + value_boost
+      unallocated_players -= value_boost
+    end
+    one_player_per_pos.each do |p|
+      break if unallocated_players.zero?
+      value_boost = 1
+      positions_count[p.key] = p.value + value_boost
+      unallocated_players -= value_boost
+    end
+    positions_count
+  end
 end
--- app/services/create_new_team_service.rb
+++ app/services/create_new_team_service.rb
@@ -3,14 +3,14 @@ class CreateNewTeamService
     team = user.team.build(team_params)
 
     # todo add randomization with :formation
-    Sponsor.create_rand(@team.id)
-    Generator::RandomTeam.new(@team).generate
-    @team.operations.create(
+    Sponsor.create_rand(team.id)
+    Generator::RandomTeam.new(team).generate
+    team.operations.create(
       sum: 250_000.0,
       kind: true,
       title: I18n.t('messages.operation.init')
     )
-    @team.captain.update(captain: true)
+    team.captain.update(captain: true)
 
     team
   end
