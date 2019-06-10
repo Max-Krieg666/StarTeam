acd94d6188ecd38859c54e41ae3a1b1be7d75944
Maxim Bogoyavlenskiy <m.bogoyavlenskiy@balance-pl.ru>
2019-06-10 16:48:09 +0300

    upd lesothian & solomon islands names & lastnames

--- app/models/generator/random_name.rb
+++ app/models/generator/random_name.rb
@@ -292,8 +292,8 @@ module Generator
         file_lastnames = 'latvian'
       when 104
         # Лесото
-        file_names = 'lesotho'
-        file_lastnames = 'lesotho'
+        file_names = 'lesothian'
+        file_lastnames = 'lesothian'
       when 105
         # Либерия
         file_names = 'liberian'
